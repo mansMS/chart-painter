7dc1fad3db43119bf33e4edd80087f6cb007c528
mansMS <47422885+mansMS@users.noreply.github.com>
2019-11-27 02:37:38 +0300

    Updates

---
+++ precache-manifest.68655cee496c27845966be445229d8bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577832f2a5ab999c24e3b423b5cc3ed9",
+    "url": "/chart-painter/index.html"
+  },
+  {
+    "revision": "3d2a2bb5a6f3e5e30bf1",
+    "url": "/chart-painter/static/css/main.967a75b9.chunk.css"
+  },
+  {
+    "revision": "8e8f8024a391354ba968",
+    "url": "/chart-painter/static/js/2.119331d9.chunk.js"
+  },
+  {
+    "revision": "3d2a2bb5a6f3e5e30bf1",
+    "url": "/chart-painter/static/js/main.bcb55828.chunk.js"
+  },
+  {
+    "revision": "0c8eccfb53e541d62be1",
+    "url": "/chart-painter/static/js/runtime-main.a8de3a73.js"
+  }
+]);
